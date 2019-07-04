310879f88d3d882788e6b0f9e2bd6ea1a3786225
Ian Wright <ianwright@lx535esg.york.ac.uk>
2019-07-04 09:31:43 +0100

    updating tests

--- tests/HelloStarWarsHooks.unit.js
+++ tests/HelloStarWarsHooks.unit.js
@@ -1,52 +1,55 @@
 import React from "react";
 import renderer from 'react-test-renderer';
-import Adapter from "enzyme-adapter-react-16";
-import enzyme from "enzyme";
-import { mount } from "enzyme";
 import { act } from 'react-dom/test-utils';
+import { render } from '@testing-library/react';
 
 import HelloStarWars from "../src/HelloStarWarsHooks";
 import useFetch from "../src/useFetch";
 
-const mockFetch = jest.fn();
-jest.mock("../src/useFetch", () => () => mockFetch());
-
-enzyme.configure({ adapter: new Adapter() });
+//const mockFetch = jest.fn();
+//jest.mock("../src/useFetch", () => () => mockFetch());
 
 describe("HelloStarWarsHooks", () => {
 
-	xit("Renders with a place holder", () => {
-		const result = renderer
-			.create(<HelloStarWars id="1" />)
-			.toJSON();
+	it("Renders with a place holder", () => {
+		// Render our component
+		const { asFragment } = render(<HelloStarWars id="1" />);
 
-		expect(result).toMatchSnapshot();
+		expect(asFragment()).toMatchSnapshot();
 	});
 
 	describe("ComponentDidMount", () => {
-
+		
 		beforeEach(() => {
 			fetch.resetMocks();
 		});
 
 		it("Obtains result and sets data correctly", async () => {
-			const wrapper = await mount(<HelloStarWars id="1" />);
-            const instance = wrapper.instance();
-            
-            mockFetch.mockImplementationOnce(() => Promise.resolve(({ name: "Jedi Master"})));
+			let result;
 
-			wrapper.update();
-			expect(wrapper).toMatchSnapshot();
+			fetch.mockResponseOnce(JSON.stringify({ name: 'Jedi Master' }));
+
+			// Render our component within an act to flush the useFetch
+			act(() => {
+				const { asFragment } = render(<HelloStarWars id="1" />);
+				result = asFragment();
+			});
+
+			expect(result).toMatchSnapshot();
 		});
 
-		xit("Handles an error in the web request correctly", async () => {
-			const wrapper = await mount(<HelloStarWars id="foobar" />);
-			const instance = wrapper.instance();
+		it("Handles an error in the web request correctly", async () => {
+			let result;
 
-            mockFetch.mockImplementationOnce(() => Promise.reject("Invalid ID"));
+			fetch.mockReject(new Error("Invalid ID"));
 
-			wrapper.update();
-			expect(wrapper).toMatchSnapshot();
+			// Render our component within an act to flush the useFetch
+			act(() => {
+				const { asFragment } = render(<HelloStarWars id="foobar" />);
+				result = asFragment();
+			});
+			
+			expect(result).toMatchSnapshot();
 		});
 	});
 });
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,10 @@ module.exports = {
 			test: /\.(js|jsx)$/,
 			exclude: /node_modules/,
 			use: {
-				loader: "babel-loader"
+				loader: "babel-loader",
+				options: {
+					presets: ["react"]
+				}
 			}
 		}]
 	}
